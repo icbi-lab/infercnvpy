8dd606a44521435a58bd48b3ce716777c47d0869
grantn5 <grant@cosyne.com>
2024-06-12 17:44:44 +0100

    changing scanpy.tl.pca to sc.tl.pca to get build docs to work

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,6 @@ html_context = {
 # Add any Sphinx extension module names here, as strings.
 # They can be extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
-    "scanpy",
     "myst_nb",
     "sphinx_copybutton",
     "sphinx.ext.autodoc",
